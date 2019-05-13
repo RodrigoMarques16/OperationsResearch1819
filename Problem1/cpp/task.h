c50d5efa1c49d3bc25393f7ae06b588a14eab40e
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2019-05-13 15:09:23 +0100

    docs

--- Problem1/cpp/task.h
+++ Problem1/cpp/task.h
@@ -8,43 +8,67 @@
 
 namespace mad {
 
+/**
+ * @brief Describes a task
+ *
+ * Structure to describe a task with duration, number of workers and dependant
+ * tasks.
+ */
 struct Task {
+
     int id;
     int duration;
     int workers;
-    std::vector<int> successors;
 
-    int in_degree = 0;
+    int in_degree  = 0;
     int out_degree = 0;
 
-    Task() = default;
+    std::vector<int> successors;
+
+    Task()  = default;
     ~Task() = default;
 
-    // Constructor
-    Task(const int& id, const int& duration, const int& workers,
+    /**
+     * @brief Default constructor
+     *
+     * @param id
+     * @param duration
+     * @param workers
+     * @param successors
+     */
+    Task(const int& id,
+         const int& duration,
+         const int& workers,
          const std::vector<int>& successors)
-        : id(id),
-          duration(duration),
-          workers(workers),
-          successors(successors) {}
+        : id(id), duration(duration), workers(workers), successors(successors) {
+    }
 
-   Task(const Task& other) = default;
+    /**
+     * @brief Copy constructor
+     *
+     * @param other
+     */
+    Task(const Task& other) = default;
 
+    /**
+     * @brief Print a task
+     *
+     * Prints a task in a single line
+     */
     void print() {
         std::cout << '\t' << id << ": "
                   << "  duration: " << duration << " workers: " << workers
                   << " succeded by: ";
-        for (auto& succ : successors) 
+        for (auto& succ : successors)
             std::cout << succ << " ";
         std::cout << "\n";
     }
-    
+
     friend bool operator<(const Task& lhs, const Task& rhs) {
         return lhs.duration < rhs.duration;
     }
-
 };
 
-}  // namespace mad
+} // namespace mad
 
 #endif
